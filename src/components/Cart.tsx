f3db1838f0d6a7cbd80a086dcea98125114befed
Ozor Michael Chibuike <buikemichael@gmail.com>
2022-04-18 16:11:31 +0100

    update cart component typescript

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,47 +1,48 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { ChevronRight } from 'react-feather';
 import { toggleCartSideBar } from '../redux/action';
 import GetTotalAmount from './GetTotalAmount';
 import GetCurrencies from './GetCurrencies';
 import CartItem from './CartItem';
-interface State{
-    currency:string,
-    cart:[],
+interface State {
+  currency: string,
+  cart: [],
 }
-interface Cart{
-    length?:number
+interface Props {
+  length?: number
 }
-export default function Cart() {
-    const cart:Cart = useSelector((state:State) => state.cart)
-    const dispatch = useDispatch()
+const Cart: FC<Props> = () => {
+  const cart = useSelector((state: State) => state.cart)
+  const dispatch = useDispatch()
 
-    const handleCloseSideBar = () => {
-        dispatch(toggleCartSideBar())
-    }
-
-    return (
-        <>
-            <div className="cart">
-                <div className="cart-inner">
-                    <div className="title">
-                        <div>
-                            <button onClick={() => handleCloseSideBar()} className="closeButton"><div><ChevronRight size={20} strokeWidth={1} /></div></button>
-                        </div>
-                        <h5>YOUR CART</h5>
-                        <div></div>
-                    </div>
-                    <div className="currency-holder">
-                        <GetCurrencies />
-                    </div>
-                    <div className="cart-list-holder">
-                        <div className="cart-list">
-                            <CartItem />
-                        </div>
-                    </div>
-                    {cart.length ? <GetTotalAmount /> : false}
-                </div>
+  const handleCloseSideBar = () => {
+    dispatch(toggleCartSideBar())
+  }
+  return (
+    <>
+      <div className="cart">
+        <div className="cart-inner">
+          <div className="title">
+            <div>
+              <button onClick={() => handleCloseSideBar()} className="closeButton"><div><ChevronRight size={20} strokeWidth={1} /></div></button>
             </div>
-        </>
-    )
+            <h5>YOUR CART</h5>
+            <div></div>
+          </div>
+          <div className="currency-holder">
+            <GetCurrencies />
+          </div>
+          <div className="cart-list-holder">
+            <div className="cart-list">
+              <CartItem />
+            </div>
+          </div>
+          {cart.length ? <GetTotalAmount /> : false}
+        </div>
+      </div>
+    </>
+  )
 }
+
+export default Cart
